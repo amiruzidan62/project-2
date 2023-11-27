8c6b414ea3b2183c0a5cf8c8b4b19c0d558828c4
amiruzidan62 <amiruzidan62@gmail.com>
2023-11-27 21:22:22 +0700

    init

---
+++ src/pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Ini About Page</div>
+</template>
---
+++ src/pages/restricted.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis id odit distinctio, amet voluptatem similique nulla quis error iure sapiente dolore. Sit aspernatur eum magnam blanditiis. Perferendis dicta accusamus nihil.
+    </div>
+</template>
